d87e3079aae0d364c179b806970d2be0f99385c2
lemonsuqing <1428828189@qq.com>
2023-12-07 15:23:36 +0800

    framework 3.0 : implement pstreeName & pid & ppid

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -23,41 +23,40 @@ void Print(void);
 int main(int argc, char *argv[]) {
   char c;
   if (strcmp(argv[1], "pstree") != 0) {
-    printf("If you want to query the process tree, output the 'pstree' command.\nIf it's not querying the process tree, then I can't help you.\n");
-    //如果你想查询进程树，请输出‘pstree’指令。\n如果不是查询进程树，那我帮不了你。
+    printf("If you want to query the process tree, output the 'pstree' command.\n");
     return 1;
 }
 
   while((c = getopt_long(argc, argv, "npV", long_options, NULL)) != -1){
     switch(c){
       case 'n':
-        nflag=1;
-        break;
+        nflag=1;  break;
       case 'p':
-        pflag=1;
-        break;
+        pflag=1;  break;
       case 'V':
-        vflag=1;
-        break;
+        vflag=1;  break;
       case 0:
-        break;//若是flag字段表示NULL时，getopt_long()会返回0，啥也不做。
+        break;//若是flag字段表示NULL时，getopt_long()会返回0
       case '?':
         printf("not this option:%s\n",argv[optind-1]);//当getopt_long()函数遇到一个未知的选项时，它会返回'?'
-        //这时输出未知选项.
+        //这时输出未知的那个选项.
         return 1;
       default:
         printf("not this program\n");
         break;
     }
   }
-  Print();
+  if(vflag){
+    printf("pstree -V\n");
+  }
 
   DIR *dirp;
   struct dirent *dp,*ptr;
   FILE *fp;
   char filepath[512];
   char procname[256];
-
+  char line[256];
+  int ppid;
   
   dirp = opendir("/proc");
   if (dirp == NULL) {
@@ -65,19 +64,22 @@ int main(int argc, char *argv[]) {
       return 1;
   }
 
-  printf("PID\n");
   while ((dp = readdir(dirp)) != NULL) {
       if (dp->d_type == DT_DIR && dp->d_name[0] >= '0' && dp->d_name[0] <= '9') {
-          //printf("%s\n", dp->d_name);
           // 打开/proc/[PID]/status文件
           snprintf(filepath, sizeof(filepath), "/proc/%s/status", dp->d_name); 
           fp = fopen(filepath, "r");
           if (fp != NULL) {
-              // 读取进程名称
-              fscanf(fp, "Name:\t%s", procname);
-              printf("%s(%s)\n", procname, dp->d_name);
-              fclose(fp);
-          }
+                while (fgets(line, sizeof(line), fp)) {
+                    if (strncmp(line, "Name:", 5) == 0) {
+                        sscanf(line, "Name:\t%s", procname);
+                    } else if (strncmp(line, "PPid:", 5) == 0) {
+                        sscanf(line, "PPid:\t%d", &ppid);
+                    }
+                }
+                printf("%s(%s)\t%d\n", procname, dp->d_name, ppid);
+                fclose(fp);
+            }
       }
   }
 
@@ -85,26 +87,4 @@ int main(int argc, char *argv[]) {
 
   assert(argv[argc] == NULL);
   return 0;
-}
-
-void Print(){
-  if(nflag){
-    pstree_n();
-  }
-  if(pflag){
-    pstree_p();
-  }
-  if(vflag){
-    pstree_V();
-  }
-}
-
-void pstree_n(void){
-  printf("pstree -n\n");
-}
-void pstree_p(void){
-  printf("pstree -p\n");
-}
-void pstree_V(void){
-  printf("pstree -V\n");
 }
