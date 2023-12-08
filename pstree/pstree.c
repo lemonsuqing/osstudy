c1416c787c95031b34022ad95bf16ac61eac5a8c
lemonsuqing <1428828189@qq.com>
2023-12-08 10:48:00 +0800

    Over? To be continued...

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -4,6 +4,7 @@
 #include <sys/types.h>
 #include <dirent.h>//使用opendir和readdir函数
 #include <string.h>
+#include <stdlib.h>
 
 int nflag=0,pflag=0,vflag=0;
 
@@ -14,11 +15,73 @@ struct option long_options[]={
 	{0, 0, 0, 0}
 };
 
-void pstree_n(void);
-void pstree_p(void);
-void pstree_V(void);
+typedef struct Proc{
+  char procName[256];
+  int Pid;
+  int PPid;
+  struct Proc *next;
+  struct Proc *child;
+}Proc;
 
-void Print(void);
+#define MAX_PROCS  791621423  // 最大进程数
+int flag = 1;
+Proc* procs[MAX_PROCS];  // 存储所有进程的哈希表
+
+Proc* createProc(char* procName, int Pid, int PPid) {
+    Proc* proc = (Proc*)malloc(sizeof(Proc));
+    strcpy(proc->procName, procName);
+    proc->Pid = Pid;
+    proc->PPid = PPid;
+    proc->next = NULL;
+    proc->child = NULL;
+    return proc;
+}
+
+void insertProc(Proc* proc) {
+    procs[proc->Pid] = proc;
+    if (proc->PPid != 0 && procs[proc->PPid] != NULL) {
+        Proc* current = procs[proc->PPid]->child;
+        Proc* prev = NULL;
+        while (current != NULL && current->Pid < proc->Pid) {
+            prev = current;
+            current = current->next;
+        }
+        if (prev == NULL) {
+            proc->next = procs[proc->PPid]->child;
+            procs[proc->PPid]->child = proc;
+        } else {
+            proc->next = prev->next;
+            prev->next = proc;
+        }
+    }
+}
+
+
+void printProc(Proc* proc, int level) {
+    if (flag) {
+        flag = 0;
+    } else {
+        for (int i = 0; i < level; i++) {
+            printf("  ");
+        }
+        printf("|\n");
+    }
+    for (int i = 0; i < level; i++) {
+    printf("  ");
+    }
+    if (level > 0) {
+        printf("+--");
+    }
+    if(pflag)
+      printf("%s(%d)\n", proc->procName, proc->Pid);
+    else
+      printf("%s\n", proc->procName);
+    Proc* child = proc->child;
+    while (child != NULL) {
+        printProc(child, level + 1);
+        child = child->next;
+    }
+}
 
 int main(int argc, char *argv[]) {
   char c;
@@ -48,6 +111,7 @@ int main(int argc, char *argv[]) {
   }
   if(vflag){
     printf("pstree -V\n");
+    return 0;
   }
 
   DIR *dirp;
@@ -57,6 +121,7 @@ int main(int argc, char *argv[]) {
   char procname[256];
   char line[256];
   int ppid;
+  int count = 0;
   
   dirp = opendir("/proc");
   if (dirp == NULL) {
@@ -77,14 +142,18 @@ int main(int argc, char *argv[]) {
                         sscanf(line, "PPid:\t%d", &ppid);
                     }
                 }
-                printf("%s(%s)\t%d\n", procname, dp->d_name, ppid);
+                Proc* proc = createProc(procname, atoi(dp->d_name), ppid);
+                insertProc(proc);
+                //printf("%s(%s)\t%d\n", procname, dp->d_name, ppid);
+                count ++;
                 fclose(fp);
             }
       }
   }
 
   closedir(dirp);
-
+  printf("Total proc: %d\n",count);
+  printProc(procs[1], 0);
   assert(argv[argc] == NULL);
   return 0;
 }
